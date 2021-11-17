6c19e887c431d99c370bf14e79e0d3657aeb4140
Martin Fabregues <martinfabregues@hotmail.com>
2021-11-17 00:20:32 -0300

    Conexión a base de datos funcionando. Cambio en tabla Clientes de base de datos.

--- php/datosDB.php
+++ php/datosDB.php
@@ -1,16 +1,8 @@
-<?php// apertura de php
+<?php
+define('SERVER', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB', 'der_devplant');
+$conector = mysqli_connect(SERVER, USER, PASS, DB);
 
-$host = "localhost";// servidor remoto
-$usuario = "root";// usuario que asignemos a BD
-$clave = "root";// clave que inventamos
-$basededatos = "dev_devplant";// Nombre de la base de datos
-
-$conn = new mysqli($host,$usuario,$clave,$basededatos); // variables + consulta.
-mysqli_query($conn , "SET character_set_result=utf8");//para que los datos se lean en utf8.
-
-if ($conn ->connect_error){ // preguntamos si la relacion tuvo error.
-         die( "Database Error : " . $conn ->connect_error); // si hay error revisar datos de variables.
-
-}
-
-?>
+?> 
