4119118615a81e0edf432064d71e30bb457b0a0c
manuels12345 <92234244+manuels12345@users.noreply.github.com>
2022-08-15 19:58:58 -0300

    lalalala

--- DevPlant/src/app/pages/pages.module.ts
+++ DevPlant/src/app/pages/pages.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { LayoutsModule } from '../layouts/layouts.module';
+import { ECommerceComponent } from './components/e-commerce/e-commerce.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ECommerceComponent
   ],
   imports: [
     CommonModule,
     LayoutsModule
   ],
   exports: [
-    HomeComponent
+    HomeComponent,
+    ECommerceComponent
   ]
 })
 export class PagesModule { }
