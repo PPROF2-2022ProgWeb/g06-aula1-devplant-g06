b8e9adb6b9867958ef69ec8d1cc2816a7e5e947b
Martin Fabregues <martinfabregues@hotmail.com>
2022-08-16 12:09:34 -0300

    prueba modulo e-commerce + modulo cart

--- DevPlant/src/app/app.module.ts
+++ DevPlant/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
+import { ECommerceModule } from './e-commerce/e-commerce.module';
 
 
 
@@ -14,7 +15,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    ECommerceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
