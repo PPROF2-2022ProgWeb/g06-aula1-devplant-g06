60023e3d755aca0de264198a55fd272a6e38a324
Martin Fabregues <martinfabregues@hotmail.com>
2022-08-16 10:56:03 -0300

    arreglando las cosas que hace MAnu

--- DevPlant/src/app/pages/pages.module.ts
+++ DevPlant/src/app/pages/pages.module.ts
@@ -2,27 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { LayoutsModule } from '../layouts/layouts.module';
-<<<<<<< HEAD
-
-=======
-import { ECommerceComponent } from './components/e-commerce/e-commerce.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
->>>>>>> 06fd021fbeb173b881a655411006b37748893cc1
-
-
-
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
-    HomeComponent
-   
-=======
     HomeComponent,
-    ECommerceComponent,
-    ContactUsComponent
-
->>>>>>> 06fd021fbeb173b881a655411006b37748893cc1
+    ContactUsComponent,
   ],
   imports: [
     CommonModule,
@@ -31,12 +16,7 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
   
   exports: [
     HomeComponent,
-<<<<<<< HEAD
-    
-=======
-    ECommerceComponent,
     ContactUsComponent
->>>>>>> 06fd021fbeb173b881a655411006b37748893cc1
   ]
 })
 export class PagesModule { }
