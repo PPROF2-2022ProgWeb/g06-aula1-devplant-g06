edff80d491dc6a50d3aa236f44721d726082e173
Martin Fabregues <martinfabregues@hotmail.com>
2022-08-16 15:20:04 -0300

    b

--- DevPlant/src/app/app.module.ts
+++ DevPlant/src/app/app.module.ts
@@ -4,21 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
-import { CartComponent } from './e-commerce/cart/cart.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PagesModule,
-    ECommerceModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
