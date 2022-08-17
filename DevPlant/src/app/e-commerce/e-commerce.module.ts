d53e42e65440a908ec751c3d62c3d9559c44692b
Martin Fabregues <martinfabregues@hotmail.com>
2022-08-17 15:51:43 -0300

    componente tienda dentro de modulo e-commerce

--- DevPlant/src/app/e-commerce/e-commerce.module.ts
+++ DevPlant/src/app/e-commerce/e-commerce.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CartComponent } from './cart/cart.component';
+import { TiendaComponent } from './tienda/tienda.component';
 
 
 
 @NgModule({
   declarations: [
-    CartComponent
+    CartComponent,
+    TiendaComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    CartComponent
+    CartComponent,
+    TiendaComponent
     
   ]
 })
