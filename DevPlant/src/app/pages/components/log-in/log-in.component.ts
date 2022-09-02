6d843505006ee8e068251f2fec74e4868e12ae34
manuels12345 <92234244+manuels12345@users.noreply.github.com>
2022-09-02 02:20:28 -0300

    Acomodando login y registro

--- DevPlant/src/app/pages/components/log-in/log-in.component.ts
+++ DevPlant/src/app/pages/components/log-in/log-in.component.ts
@@ -12,24 +12,24 @@ export class LogInComponent implements OnInit {
  form: FormGroup = new FormGroup({});
   constructor(
     private fb: FormBuilder){
-      
+
      }
 
   ngOnInit(): void {
    this.CreateForm();
   }
-  
+
   CreateForm(){
     this.form = this.fb.group({
 
       username:[" ",[Validators.required, Validators.email,Validators.minLength(10)]],
-      
+
       password:[" ",[Validators.required,Validators.minLength(8)]],
 
     });
   }
   logIn(){
-   
+
 
   }
 
