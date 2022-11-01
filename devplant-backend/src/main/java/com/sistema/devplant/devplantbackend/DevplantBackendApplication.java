8bbf09c70bf43753a87cdf7d611c10f65baef4d8
manuels12345 <92234244+manuels12345@users.noreply.github.com>
2022-11-01 17:15:05 -0300

    lalala

---
+++ devplant-backend/src/main/java/com/sistema/devplant/devplantbackend/DevplantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.devplant.devplantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevplantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevplantBackendApplication.class, args);
+	}
+
+}
