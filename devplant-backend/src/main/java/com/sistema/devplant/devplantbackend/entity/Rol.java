7b94a28360c09d9a79e56e2d544ce8919e6df71a
manuels12345 <92234244+manuels12345@users.noreply.github.com>
2022-11-01 19:30:56 -0300

    Empezando el proyecto Spring

---
+++ devplant-backend/src/main/java/com/sistema/devplant/devplantbackend/entity/Rol.java
@@ -0,0 +1,48 @@
+package com.sistema.devplant.devplantbackend.entity;
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "roles")
+public class Rol {
+
+    @Id
+    private long rolId;
+    private String nombre;
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "usuario")
+    private Set<UsuarioRol> usuarioRoles = new HashSet<>();
+
+    public long getRolId() {
+        return rolId;
+    }
+
+    public void setRolId(long rolId) {
+        this.rolId = rolId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<UsuarioRol> getUsuarioRoles() {
+        return usuarioRoles;
+    }
+
+    public void setUsuarioRoles(Set<UsuarioRol> usuarioRoles) {
+        this.usuarioRoles = usuarioRoles;
+    }
+
+    public Rol(){
+
+    }
+
+}
---
+++ devplant-backend/src/main/java/com/sistema/devplant/devplantbackend/repository/RolRepository.java
@@ -0,0 +1,10 @@
+package com.sistema.devplant.devplantbackend.repository;
+
+import com.sistema.devplant.devplantbackend.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol,Long> {
+
+
+
+}
